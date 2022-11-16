dbb35c22802f30509ca1e89c0eda63f392dab2c2
Orcohen33 <orcohen3322@gmail.com>
2022-11-16 12:27:37 +0200

    Finish 'Company' controller

--- backend/src/main/java/com/vouchit/backend/controller/CompanyController.java
+++ backend/src/main/java/com/vouchit/backend/controller/CompanyController.java
@@ -1,13 +1,14 @@
 package com.vouchit.backend.controller;
 
+import com.vouchit.backend.model.request.CompanyRequest;
 import com.vouchit.backend.model.response.CompanyResponse;
 import com.vouchit.backend.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/company")
@@ -20,8 +21,25 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
+    // Get all companies
+    @GetMapping
+    public ResponseEntity<Set<CompanyResponse>> getAllCompanies() {
+        return ResponseEntity.ok(companyService.getAllCompanies());
+    }
+
+    // Get company by id
     @GetMapping("/{id}")
     public ResponseEntity<CompanyResponse> getCompanyById(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(companyService.getCompanyById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CompanyResponse> createCompany(@RequestBody CompanyRequest companyRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(companyService.createCompany(companyRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCompany(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(companyService.deleteCompany(id));
     }
 }
