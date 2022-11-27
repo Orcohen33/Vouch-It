7f0f4533aa2f30333ea36b99152c275bcc4b10c6
ronipick <ronipick3@gmail.com>
2022-11-27 16:55:12 +0200

    add SignupActivity

---
+++ MyApplication/app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        signup();
+    }
+
+    private void signup() {
+//        TextInputEditText
+    }
+}
