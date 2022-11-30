2d36196d3d74983eeb7aa7d4aa1f12b5e07424f1
Orcohen33 <orcohen3322@gmail.com>
2022-11-30 13:06:28 +0200

    changed names to files. (I deleted the previous MainActivity and renamed NavigationBarActivity to MainActivity)

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_bar);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        findViewById(R.id.menu_image_view).setOnClickListener(v -> {
+            drawerLayout.openDrawer(GravityCompat.END);
+            System.out.println("Menu clicked");
+        });
 
 
 
