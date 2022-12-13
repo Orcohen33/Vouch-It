31a422bbc00df396e26c63b4e27553453bee8278
Orcohen33 <orcohen3322@gmail.com>
2022-12-13 19:43:28 +0200

    The error messages that appeared when filling in payment details have been fixed.

--- MyApplication/app/src/main/java/com/example/myapplication/activities/PaymentActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/activities/PaymentActivity.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -16,6 +15,8 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paymentpage);
+        pay();
+
     }
     public void pay(){
         EditText cardNumber = findViewById(R.id.credit_card_number);
@@ -23,37 +24,60 @@ public class PaymentActivity extends AppCompatActivity {
         EditText expiryMonth = findViewById(R.id.expiration_month);
         EditText expiryYear = findViewById(R.id.expiration_year);
         EditText cvv = findViewById(R.id.cvv);
-        String cardNumberString = cardNumber.getText().toString();
-        String cardHolderNameString = cardHolderName.getText().toString();
-        String expiryMonthString = expiryMonth.getText().toString();
-        String expiryYearString = expiryYear.getText().toString();
-        String cvvString = cvv.getText().toString();
-        if (cardNumberString.length() != 16) {
-            cardNumber.setError("Invalid card number");
-        }
-        if (cardHolderNameString.length() == 0) {
-            cardHolderName.setError("Invalid card holder name");
-        }
-        if (expiryMonthString.length() != 2) {
-            expiryMonth.setError("Invalid expiry month");
-        }
-        if (expiryYearString.length() != 4) {
-            expiryYear.setError("Invalid expiry year");
-        }
-        if (cvvString.length() != 3) {
-            cvv.setError("Invalid cvv");
-        }
-//
-//        if(cardNumberString.isEmpty() || cardHolderNameString.isEmpty() || expiryYearString.isEmpty() || expiryMonthString.isEmpty() || cvvString.isEmpty()){
-//            return;
-//        }
-//
-//        if(Integer.parseInt(expiryMonthString) > 12 || Integer.parseInt(expiryMonthString) < 1){
-//            return;
-//        }
-//        Intent intent = new Intent(this, CustomerActivity.class);
-//        startActivity(intent);
 
+        // Check validation of the fields
+        EditText.OnFocusChangeListener onFocusChangeListenerCardNumber = (v, hasFocus) -> {
+            if (!hasFocus) {
+                EditText editText = (EditText) v;
+                if (editText.getText().length() != 16) {
+                    editText.setError("Credit card number must be 16 digits");
+                }
+            }
+        };
+        cardNumber.setOnFocusChangeListener(onFocusChangeListenerCardNumber);
+
+        EditText.OnFocusChangeListener onFocusChangeListenerCardHolderName = (v, hasFocus) -> {
+            if (!hasFocus) {
+                EditText editText = (EditText) v;
+                if (editText.getText().length() < 3) {
+                    editText.setError("Card holder name must be at least 3 characters");
+                }
+            }
+        };
+        cardHolderName.setOnFocusChangeListener(onFocusChangeListenerCardHolderName);
+
+        View.OnFocusChangeListener onFocusChangeListenerExpiryMonth = (v, hasFocus) -> {
+            if (!hasFocus) {
+                EditText editText = (EditText) v;
+                if (editText.getText().toString().isEmpty() || (editText.getText().length() != 2 ||
+                        (Integer.parseInt(editText.getText().toString()) > 12 ||
+                        Integer.parseInt(editText.getText().toString()) < 1))) {
+                    editText.setError("- Expiry month must be 2 digits\n- Expiry month must be between 1 and 12");
+                }
+            }
+        };
+        expiryMonth.setOnFocusChangeListener(onFocusChangeListenerExpiryMonth);
+
+        View.OnFocusChangeListener onFocusChangeListenerExpiryYear = (v, hasFocus) -> {
+            if (!hasFocus) {
+                EditText editText = (EditText) v;
+                if (editText.getText().toString().isEmpty() ||
+                        editText.getText().length() != 4 || Integer.parseInt(editText.getText().toString()) < 2021) {
+                    editText.setError("- Expiry year must be 4 digits\n- Expiry year must be at least 2021");
+                }
+            }
+        };
+        expiryYear.setOnFocusChangeListener(onFocusChangeListenerExpiryYear);
+
+        View.OnFocusChangeListener onFocusChangeListenerCvv = (v, hasFocus) -> {
+            if (!hasFocus) {
+                EditText editText = (EditText) v;
+                if (editText.getText().length() != 3) {
+                    editText.setError("CVV must be 3 digits");
+                }
+            }
+        };
+        cvv.setOnFocusChangeListener(onFocusChangeListenerCvv);
     }
     public void onBackPaymentImageClick(View view) {
         Intent intent = new Intent(this, AddToCartActivity.class);
