eb6fd155669d76f16aaa4d004d5827d52049fd91
Orcohen33 <orcohen3322@gmail.com>
2022-12-24 12:27:40 +0200

    Changed analysis data view.

--- MyApplication/app/src/main/java/com/example/myapplication/fragments/company/analysis/AnalysisFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/fragments/company/analysis/AnalysisFragment.java
@@ -15,10 +15,16 @@ import android.view.ViewGroup;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentAnalysisBinding;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,16 +45,57 @@ public class AnalysisFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(AnalysisViewModel.class);
 
         PieChart pieChart = binding.pieChart;
+        loadPieChartData(pieChart);
+
+        BarChart barChart = binding.barChart;
+        loadBarChartData(barChart);
+
+        return binding.getRoot();
+    }
+
+    private void loadBarChartData(BarChart barChart) {
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1, 10));
+        entries.add(new BarEntry(2, 20));
+        entries.add(new BarEntry(3, 30));
+        entries.add(new BarEntry(4, 40));
+
+        BarDataSet barDataSet = new BarDataSet(entries, "Bar Data Set");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        barChart.setFitBars(true);
+        barChart.setData(new BarData(barDataSet));
+        barChart.getDescription().setText("Bar Chart Description");
+        barChart.animateY(2000);
+
+    }
+
+    private static void loadPieChartData(PieChart pieChart) {
         List<PieEntry> pieEntries = new ArrayList<>();
-        pieEntries.add(new PieEntry(10, "A"));
-        pieEntries.add(new PieEntry(20, "B"));
-        pieEntries.add(new PieEntry(30, "C"));
+        pieEntries.add(new PieEntry(0.2f, "A"));
+        pieEntries.add(new PieEntry(0.10f, "B"));
+        pieEntries.add(new PieEntry(0.3f, "C"));
+        pieEntries.add(new PieEntry(0.4f, "D"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int c : ColorTemplate.MATERIAL_COLORS) {
+            colors.add(c);
+        }
+        for (int c : ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(c);
+        }
 
         PieDataSet dataSet = new PieDataSet(pieEntries, "Categories");
         // set colors to be ColorTemplate.MATERIAL_COLORS
-        dataSet.setColors(R.color.purple_200, R.color.purple_500, R.color.purple_700);
+        dataSet.setColors(colors);
 
         PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(13f);
+        data.setValueTextColor(Color.WHITE);
         pieChart.setData(data);
 
         // Customize the chart's styling options
@@ -56,13 +103,13 @@ public class AnalysisFragment extends Fragment {
         pieChart.getDescription().setEnabled(false);
         pieChart.setExtraOffsets(5, 10, 5, 5);
         pieChart.setDrawHoleEnabled(false);
+        pieChart.setCenterTextSize(30);
+        pieChart.setEntryLabelTextSize(20);
         pieChart.setHoleColor(Color.WHITE);
         pieChart.setTransparentCircleRadius(61f);
 
         // Update the chart
         pieChart.invalidate();
-
-        return binding.getRoot();
     }
 
 }
--- MyApplication/app/src/main/java/com/example/myapplication/fragments/company/analysis/AnalysisViewModel.java
+++ MyApplication/app/src/main/java/com/example/myapplication/fragments/company/analysis/AnalysisViewModel.java
@@ -4,4 +4,5 @@ import androidx.lifecycle.ViewModel;
 
 public class AnalysisViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+
 }
